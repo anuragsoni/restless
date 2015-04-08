42e61b4e95e279f50c658799bf79f549128a80d1
Anurag Soni <anuragsoni.13@gmail.com>
2015-04-07 21:48:37 -0400

    Http::Get Test: ===============
    Add test case for the simple HTTP Get method

--- test/test.cpp
+++ test/test.cpp
@@ -2,20 +2,11 @@
 #include "catch.hpp"
 #include <restless.hpp>
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
-}
-
-TEST_CASE( "Factorials are computed II", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+TEST_CASE( "Simple Get Request", "[get]" ) {
+    auto result = asoni::Handle()
+        .get("http://httpbin.org/robots.txt")
+        .exec();
+    std::string actual = "User-agent: *\nDisallow: /deny\n";
+    REQUIRE(result.code == 200);
+    REQUIRE(result.body == actual);
 }
