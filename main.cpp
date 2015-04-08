78f8c1924cc23a5858892fb8190cf13558262e43
Anurag Soni <anuragsoni.13@gmail.com>
2015-04-07 21:49:14 -0400

    simple example of how to use restless

--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
 #include <string>
 #include <iostream>
 #include <restless.hpp>
-using namespace std;
-class Person
-{
-    string m_name;
-    int m_age;
-
-public:
-    Person &name(string const &name) {this->m_name = name; return *this;}
-    Person &age(int const age) {this->m_age = age; return *this;}
-
-    friend ostream & operator << (ostream &os, Person const &);
-};
-
-
-ostream & operator << (ostream &os, Person const &person)
-{
-    return os << "Hello, my name is " << person.m_name << " and I am " << person.m_age << " years old.";
-}
-
-
 int main(void)
 {
-    Person person;
-    cout << person.name("Peter").age(21) << endl;
-
-    auto res = asoni::Handle()
-        .get("http://example.com")
+    auto result = asoni::Handle()
+        .get("http://httpbin.org/get")
         .exec();
 
-    std::cout << res.code << " " << res.body << '\n';
+    std::cout << result.body << "\n";
 
 }
