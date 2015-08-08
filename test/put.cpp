bbb23828684cd4fdf11f23238cc1b819e055331c
Anurag Soni <anuragsoni.13@gmail.com>
2015-08-07 20:25:13 -0400

    move from catch to gtest

--- test/put.cpp
+++ test/put.cpp
@@ -1,16 +1,19 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do
-                           // this in one cpp file
-#include <catch.hpp>
+#include <gtest/gtest.h>
 #include <json.hpp>
 #include <restless.hpp>
 using json = nlohmann::json;
-TEST_CASE("Simple Put Request : HTTPS", "[put]") {
-        std::string put_content = "Hello world";
-        auto result = asoni::Handle()
-                          .put("https://httpbin.org/put")
-                          .content("text/plain", put_content)
-                          .exec();
-        auto response = json::parse(result.body);
-        REQUIRE(result.code == 200);
-        REQUIRE(response["data"] == put_content);
+TEST(PutRequestTEst, PutRequestHttps) {
+  std::string put_content = "Hello world";
+  auto result = asoni::Handle()
+                    .put("https://httpbin.org/put")
+                    .content("text/plain", put_content)
+                    .exec();
+  auto response = json::parse(result.body);
+  EXPECT_EQ(result.code, 200);
+  EXPECT_EQ(response["data"], put_content);
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
